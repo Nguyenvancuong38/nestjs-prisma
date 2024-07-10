c3669ccd51feb417b72accb0b4b5e8c90962239a
Nguyen Cuong <wazza.cuong@gmail.com>
2024-07-10 13:36:48 +0700

    feat: add example value in api product and request product sample

--- src/product-sample/dto/create-product-sample.dto.ts
+++ src/product-sample/dto/create-product-sample.dto.ts
@@ -1,29 +1,54 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 import { ProductType } from "src/enums/role.enum";
 
 export class CreateProductSampleDto {
+    @ApiProperty({
+        example: 'HDT565'
+    })
     @IsNotEmpty()
     @IsString()
     name: string
 
+    @ApiProperty({
+        example: '85X123456789'
+    })
     @IsOptional()
     partNo: string
 
+    @ApiProperty({
+        example: 'description'
+    })
     @IsOptional()
     description: string
 
+    @ApiProperty({
+        example: '2024/07/10'
+    })
     @IsOptional()
     dateStart: Date
 
+    @ApiProperty({
+        example: '2024/08/10'
+    })
     @IsOptional()
     dateEnd: Date
 
+    @ApiProperty({
+        example: 'standard'
+    })
     @IsEnum(ProductType)
     type: ProductType
 
+    @ApiProperty({
+        example: 5
+    })
     @IsNumber()
     userId: number
 
+    @ApiProperty({
+        example: 1
+    })
     @IsNumber()
     modelId: number
 }
--- src/request-product-sample/dto/create-request-product-sample.dto.ts
+++ src/request-product-sample/dto/create-request-product-sample.dto.ts
@@ -1,16 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsArray, IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateRequestProductSampleDto {
+    @ApiProperty({
+        example: 'Borrow HDT565 sample'
+    })
     @IsNotEmpty()
     @IsString()
     description: string
 
+    @ApiProperty({
+        example: 5
+    })
     @IsNumber()
     authorId: number
 
+    @ApiProperty({
+        example: [1, 2, 3]
+    })
     @IsArray()
     productSampleIds: number[]
 
+    @ApiProperty({
+        example: '2024/07/10'
+    })
     @IsDate()
     createAt: Date
 }
